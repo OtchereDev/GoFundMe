b19683b85e78ed736792c08dbe2c9127eb1fe43d
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-18 23:22:24 +0000

    added component for Top raisers

---
+++ components/index/TopFundraisers.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import CardComp from '../CardComp'
+
+
+const TopFundraisers = () => {
+    return (
+        <div className="px-8 pt-14 lg:pt-20 lg:px-16 w-full">
+
+            <h2 className="text-3xl lg:text-4xl font-bold ">
+                Top Fundraiser
+            </h2>
+
+            <CardComp/>
+
+
+            
+        </div>
+    )
+}
+
+export default TopFundraisers
