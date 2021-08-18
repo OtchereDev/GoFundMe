ca44df846342fb7b18b518901ac4b9a1ceba0304
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-18 23:37:54 +0000

    added custom next config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary_green:"#02a95c",
+        primary_black:"#333",
+        secondary_black:"#666"
+      },
+      fontFamily:{
+        primary:"'Lato','san-serif'"
+      }
+    },
   },
   variants: {
     extend: {},
