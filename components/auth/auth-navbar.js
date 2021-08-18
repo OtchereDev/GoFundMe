d6311c5d9e07b02a6cb2444689d67156ef3793c9
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-18 09:23:09 +0000

    added auth Nar bar component

---
+++ components/auth/auth-navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from "next/link"
+import { useRouter } from 'next/router'
+import LogoSvg from '../LogoSvg'
+
+const AuthNavBar = () => {
+
+    const router = useRouter()
+   
+    return (
+
+        <div className="flex justify-between w-full bg-white shadow-md items-center py-4 px-4  lg:px-32 absolute top-0 left-0">
+            <LogoSvg/>
+            <div className="flex justify-center text-sm ">
+                <p className={`${router.pathname=="/sign-in"?"block":"hidden"}`}>
+                    <span className="hidden md:inline-block">
+                        Do not have an account? 
+                    </span>
+                    <span className="pl-2">
+                        <Link className="text-primary_green" href="/sign-up">
+                            <a className="text-primary_green underline">
+                                Sign Up
+                            </a>
+                        </Link>
+                    </span>
+                </p>
+
+                <p className={`${router.pathname=="/sign-up"?"block":"hidden"}`}>
+                    <span className="hidden md:inline-block">
+                       Have an account?
+                    </span>
+                    <span className="pl-2 inline-block" >
+                        <Link className="text-primary_green" href="/sign-in">
+                            <a className="text-primary_green underline">
+                                Login
+                            </a>
+                        </Link>
+                    </span>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AuthNavBar
