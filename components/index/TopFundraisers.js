d90fe70ae4a6c616f4d74b1184a4b0052e64eca9
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-19 19:57:38 +0000

    finished topfundraiser comp

--- components/index/TopFundraisers.js
+++ components/index/TopFundraisers.js
@@ -1,17 +1,42 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import CardComp from '../CardComp'
+import { faChevronRight} from "@fortawesome/free-solid-svg-icons"
 
 
 const TopFundraisers = () => {
     return (
-        <div className="px-8 pt-14 lg:pt-20 lg:px-16 w-full">
+        <div className="px-4  pt-14 lg:pt-20 lg:px-16 w-full">
 
-            <h2 className="text-3xl lg:text-4xl font-bold ">
+            <h2 className="text-3xl lg:text-4xl font-bold mb-5">
                 Top Fundraiser
             </h2>
+            
+            <div className="flex flex-wrap">
 
-            <CardComp/>
+                <CardComp/>
+                <CardComp/>
+                <CardComp/>
+                <CardComp/>
+                <CardComp/>
+
+
+            </div>
+            <div className="flex justify-end lg:my-4 ">
+                <button className="w-full border lg:hidden py-2 rounded-md my-3 border-primary_green outline-none text-primary_green text-lg ">
+                    See more
+                </button>
+
+                <button className="hidden lg:flex outline-none items-center text-gray-400 text-lg p-2">
+                    <span>
+                        See more
+                    </span>
+                    <span className="flex items-center">
+                        <FontAwesomeIcon className="text-base ml-3" icon={faChevronRight}  />
+                    </span>
+                </button>
+            </div>
 
 
             
