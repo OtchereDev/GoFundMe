46cd81d272d7e12db3ae7496c6376df26d4bb5e8
Oliver Otchere <oliverotchere4@gmail.com>
2021-08-24 13:24:27 +0000

    added next progress bar to globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import 'tailwindcss/tailwind.css'
 import "../components/assets/style.css"
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+
+        <NextNProgress color="#02a95c" />
+        <Component {...pageProps} />
+
+      </>
+        )
 }
 
 export default MyApp
