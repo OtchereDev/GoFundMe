67f80c5257159f96c5fe1cd782c572dfeb95bb27
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-04 08:44:55 +0000

    added react toastify to proj

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,16 @@ import 'tailwindcss/tailwind.css'
 import "../components/assets/style.css"
 import NextNProgress from 'nextjs-progressbar'
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function MyApp({ Component, pageProps }) {
   return (
       <>
 
         <NextNProgress color="#02a95c" />
+        <ToastContainer/>
         <Component {...pageProps} />
 
       </>
