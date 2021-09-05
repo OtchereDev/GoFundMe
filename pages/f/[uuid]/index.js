2f48d0db5d5b0c6aa946308d3f152aeff24a3b8b
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-05 20:13:18 +0000

    added data from API

--- pages/f/[uuid]/index.js
+++ pages/f/[uuid]/index.js
@@ -6,18 +6,30 @@ import Head from 'next/head'
 import Header from '../../../components/detail/Header'
 import Description from '../../../components/detail/Description'
 import Footer from "../../../components/index/Footer"
+import { APIURL } from '../../../config/config'
 
 
-const Detail = () => {
+const Detail = ({data}) => {
 
     const [showSearch,setShowSearch] = useState(false)
     const [toggler, setToggler] = useState(false)
+
+    const {amountRaised,
+        goal_amount,
+        image_url,
+        donations,
+        title,
+        createdAt,
+        organiser,
+        category,
+        description}=data
+
     return (
 
         <>
             <Head>
                 <title>
-                    Fundraiser by Chelsea Clarke : Tragic accident of a young girl
+                    Fundraiser by {data.organiser} : {data.title}
                 </title>
             </Head>
             <div className="w-full z-0 pt-14 lg:pt-20 min-h-screen">
@@ -25,9 +37,9 @@ const Detail = () => {
                 <ToggleMenu toggler={toggler} setToggler={setToggler} />
                 <NavBar  setShowSearch={setShowSearch} setToggler={setToggler} />
 
-                <Header/>
+                <Header amountRaised={amountRaised} goal_amount={goal_amount} image_url={image_url} donations={donations} />
 
-                <Description/>
+                <Description title={title} createdAt={createdAt} organiser={organiser} category={category} description={description} donations={donations} />
 
 
 
@@ -37,6 +49,21 @@ const Detail = () => {
             </div>
         </>
     )
+}
+
+export const getServerSideProps=async(req,res)=>{
+    const uuid = req.params.uuid
+    const request = await fetch(`${APIURL}/fundraiser/detail/${uuid}`)
+    
+    if (!request.ok) return {redirect:"/"}
+    const data = await request.json()
+
+
+
+    return {
+        props:{data}
+    }
+
 }
 
 export default Detail
