4597cc8be424bb54ab714b8b03666025f42b47d7
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-05 20:12:27 +0000

    added real data from API

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Header from '../components/index/Header'
 import NavBar from '../components/index/NavBar'
 import SearchComp from '../components/index/SearchComp'
@@ -9,11 +9,14 @@ import Benefit from '../components/index/Benefit'
 import NearFundraisers from '../components/index/NearFundraisers'
 import CTA from '../components/index/CTA'
 import Footer from '../components/index/Footer'
+import {APIURL} from "../config/config"
 
-export default function Home() {
+export default function Home({fundraisers}) {
 
   const [showSearch,setShowSearch] = useState(false)
   const [toggler, setToggler] = useState(false)
+  const [fundraiser] = useState([...fundraisers])
+
   return (
     <>
       <Head>
@@ -30,9 +33,9 @@ export default function Home() {
 
         <div className="w-full z-0 pt-14 lg:pt-20">
           <Header/>
-          <TopFundraisers/>
+          <TopFundraisers fundraisers={fundraiser} />
           <Benefit/>
-          <NearFundraisers/>
+          <NearFundraisers fundraisers={fundraiser} />
           <CTA/>
           <Footer/>
 
@@ -41,4 +44,16 @@ export default function Home() {
     
     </>
   )
+}
+
+export const getServerSideProps=async({req,res})=>{
+  const request = await fetch(`${APIURL}/fundraiser/`)
+
+  const data = await request.json()
+
+  return {
+    props:{
+      fundraisers:data
+    }
+  }
 }
