75a2dea6776ab92842bd8dbcf46d7b4df808cd6f
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-05 20:09:49 +0000

    added auth context to proj

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,18 @@ import NextNProgress from 'nextjs-progressbar'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import AuthContext from '../context/AuthContext';
+
 
 function MyApp({ Component, pageProps }) {
   return (
-      <>
+      <AuthContext>
 
         <NextNProgress color="#02a95c" />
         <ToastContainer/>
         <Component {...pageProps} />
 
-      </>
+      </AuthContext>
         )
 }
 
