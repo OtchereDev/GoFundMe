8ca8276c43a70c661997cc6f4bec949c35c65373
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-30 21:50:44 +0000

    finished stripe checkout form

--- components/donate/CheckoutForm.js
+++ components/donate/CheckoutForm.js
@@ -1,107 +1,169 @@
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import router from "next/router";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { APIURL } from "../../config/config";
 
-const CheckoutForm = ({totalDonation}) => {
+const CheckoutForm = ({ donationData }) => {
+  const [succeded, setSucceded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const [clientSecret, setClientSecret] = useState("");
+  const [message, setMessage] = useState("");
+  const [intentId, setIntentId] = useState();
 
-    const [succeded,setSucceded] = useState(false)
-    const [error, setError] = useState(null)
-    const [processing, setProcessing] = useState('')
-    const [disabled, setDisabled] = useState(false)
-    const [clientSecret, setClientSecret] = useState('')
+  const stripe = useStripe();
+  const element = useElements();
 
-    const stripe = useStripe()
-    const element= useElements()
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#02a95c",
+        fontFamily: "Arial, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#02a95c",
+        },
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a",
+      },
+    },
+  };
 
-    const cardStyle = {
-        style: {
-          base: {
-            color: "#02a95c",
-            fontFamily: 'Arial, sans-serif',
-            fontSmoothing: "antialiased",
-            fontSize: "16px",
-            "::placeholder": {
-              color: "#02a95c"
-            }
-          },
-          invalid: {
-            color: "#fa755a",
-            iconColor: "#fa755a"
-          }
-        }
+  useEffect(() => {
+    fetch(`${APIURL}/payments/create-payment`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ ...donationData }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setClientSecret(data?.clientSecret);
+        setIntentId(data?.intent_id);
+        // console.log("aa:", data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = async (e) => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: element.getElement(CardElement),
+      },
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceded(true);
+
+      // redirect user here to add comment orr show comment box now
     }
-    
+  };
 
-    useEffect(()=>{
-        fetch("/api/create-pament-intent",{
-            method:"POST",
-            headers:{
-                "content-type":"application/json",
-                body:JSON.stringify({amount:totalDonation})
-            }
-        })
-        .then(res=>res.json())
-        .then(data=>setClientSecret(data))
-        .catch(error=>console.log(error))
-    },[])
+  const handleAddComment = async () => {
+    try {
+      const res = await fetch(`${APIURL}/payments/${intentId}/add-comment`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
 
-    const handleChange=async(e)=>{
-        setDisabled(e.empty)
-        setError(e.error?e.error.message:"")
+      if (res.ok) {
+        toast.success("Comment sucessfully added");
+
+        setTimeout(() => {
+          router.push("/");
+        }, 1000);
+      } else {
+        console.log("rr", res);
+        const data = await res.json();
+        console.log("dd", data);
+      }
+    } catch (error) {
+      console.log("ee", error);
+      toast.error("Sorry could not add message");
     }
+  };
 
-    const handleSubmit=async(e)=>{
-        e.preventDefault()
-        setProcessing(true)
-
-        const payload= await stripe.confirmCardPayment(clientSecret,{
-            payment_method:{
-                card: element.getElement(CardElement)
-            }
-        })
-
-        if(payload.error){
-            setError(`Payment failed ${payload.error.message}`)
-            setProcessing(false)
-        }else{
-            setError(null)
-            setProcessing(false)
-            setSucceded(true)
-
-
-            // redirect user here to add comment orr show comment box now
-        }
-    }
-
-    
-    return (
-        <form id="payment-form" onSubmit={handleSubmit}>
-            <CardElement id="card-element" className="border py-3 text-lg px-2 rounded-md mb-2 border-primary_green" options={cardStyle} onChange={handleChange} />
-            <button
-                className="bg-primary_green shadow-md text-gray-50 w-full py-2 rounded-md font-semibold text-lg"
-                disabled={processing || disabled || succeded}
-                id="submit"
-            >
-                <span id="button-text">
-                {processing ? (
-                    <div className="spinner" id="spinner"></div>
-                ) : (
-                    "Pay now"
-                )}
-                </span>
-            </button>
-            {/* Show any error that happens when processing the payment */}
-            {error && (
-                <div className="card-error text-red-500" role="alert">
-                {error}
-                </div>
+  return (
+    <>
+      <form
+        id="payment-form"
+        className={`${succeded ? "hidden" : "block"}`}
+        onSubmit={handleSubmit}
+      >
+        <CardElement
+          id="card-element"
+          className="border py-3 text-lg px-2 rounded-md mb-2 border-primary_green"
+          options={cardStyle}
+          onChange={handleChange}
+          hidePostalCode={true}
+        />
+        <button
+          className="bg-primary_green shadow-md text-gray-50 w-full py-2 rounded-md font-semibold text-lg"
+          disabled={processing || disabled || succeded}
+          id="submit"
+        >
+          <span id="button-text">
+            {processing ? (
+              <div className="spinner" id="spinner"></div>
+            ) : (
+              "Pay now"
             )}
-            {/* Show a success message upon completion */}
-            <p className={succeded ? "block text-green-500" : "hidden"}>
-                Payment succeeded, Refresh the page to pay again.
-            </p>
-            </form>
-            )
-}
+          </span>
+        </button>
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+          <div className="card-error text-red-500" role="alert">
+            {error}
+          </div>
+        )}
+      </form>
+      {/* Show a success message upon completion */}
+      <div className={succeded ? "block " : "hidden"}>
+        <p className="text-green-500">
+          Payment succeeded, Please leave a message for the beneficiary
+          (optional)
+        </p>
 
-export default CheckoutForm
+        <textarea
+          className="w-full h-52 p-3 border outline-none border-green-500 font-primary resize-none rounded-md mt-4"
+          placeholder="Message*"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+
+        <button
+          className="bg-primary_green shadow-md outline-none text-gray-50 w-full py-2 rounded-md font-semibold text-lg"
+          disabled={!succeded}
+          onClick={handleAddComment}
+        >
+          Submit
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CheckoutForm;
