968215f59591c5b090cc13183fd9e058fd01d4ab
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-30 22:31:11 +0000

    added server side rendering

--- pages/f/[uuid]/index.js
+++ pages/f/[uuid]/index.js
@@ -1,69 +1,74 @@
-import React, {useState} from 'react'
-import NavBar from '../../../components/index/NavBar'
-import SearchComp from '../../../components/index/SearchComp'
-import ToggleMenu from '../../../components/index/ToggleMenu'
-import Head from 'next/head'
-import Header from '../../../components/detail/Header'
-import Description from '../../../components/detail/Description'
-import Footer from "../../../components/index/Footer"
-import { APIURL } from '../../../config/config'
+import React, { useState } from "react";
+import NavBar from "../../../components/index/NavBar";
+import SearchComp from "../../../components/index/SearchComp";
+import ToggleMenu from "../../../components/index/ToggleMenu";
+import Head from "next/head";
+import Header from "../../../components/detail/Header";
+import Description from "../../../components/detail/Description";
+import Footer from "../../../components/index/Footer";
+import { APIURL } from "../../../config/config";
 
+const Detail = ({ data }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [toggler, setToggler] = useState(false);
 
-const Detail = ({data}) => {
+  const {
+    amountRaised,
+    goal_amount,
+    image_url,
+    donations,
+    title,
+    createdAt,
+    organiser,
+    category,
+    description,
+  } = data;
 
-    const [showSearch,setShowSearch] = useState(false)
-    const [toggler, setToggler] = useState(false)
+  return (
+    <>
+      <Head>
+        <title>
+          Fundraiser by {data.organiser} : {data.title}
+        </title>
+      </Head>
+      <div className="w-full z-0 pt-14 lg:pt-20 min-h-screen">
+        <SearchComp showSearch={showSearch} setShowSearch={setShowSearch} />
+        <ToggleMenu toggler={toggler} setToggler={setToggler} />
+        <NavBar setShowSearch={setShowSearch} setToggler={setToggler} />
 
-    const {amountRaised,
-        goal_amount,
-        image_url,
-        donations,
-        title,
-        createdAt,
-        organiser,
-        category,
-        description}=data
+        <Header
+          amountRaised={amountRaised}
+          goal_amount={goal_amount}
+          image_url={image_url}
+          donations={donations}
+        />
 
-    return (
+        <Description
+          title={title}
+          createdAt={createdAt}
+          organiser={organiser}
+          category={category}
+          description={description}
+          donations={donations}
+        />
 
-        <>
-            <Head>
-                <title>
-                    Fundraiser by {data.organiser} : {data.title}
-                </title>
-            </Head>
-            <div className="w-full z-0 pt-14 lg:pt-20 min-h-screen">
-                <SearchComp showSearch={showSearch} setShowSearch={setShowSearch} />
-                <ToggleMenu toggler={toggler} setToggler={setToggler} />
-                <NavBar  setShowSearch={setShowSearch} setToggler={setToggler} />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-                <Header amountRaised={amountRaised} goal_amount={goal_amount} image_url={image_url} donations={donations} />
+export const getServerSideProps = async (req, res) => {
+  const uuid = req.params.uuid;
+  const request = await fetch(`${APIURL}/fundraiser/detail/${uuid}`);
 
-                <Description title={title} createdAt={createdAt} organiser={organiser} category={category} description={description} donations={donations} />
+  if (!request.ok) return { redirect: { permanent: false, destination: "/" } };
 
+  const data = await request.json();
 
+  return {
+    props: { data },
+  };
+};
 
-
-                <Footer/>
-
-            </div>
-        </>
-    )
-}
-
-export const getServerSideProps=async(req,res)=>{
-    const uuid = req.params.uuid
-    const request = await fetch(`${APIURL}/fundraiser/detail/${uuid}`)
-    
-    if (!request.ok) return {redirect:"/"}
-    const data = await request.json()
-
-
-
-    return {
-        props:{data}
-    }
-
-}
-
-export default Detail
+export default Detail;
