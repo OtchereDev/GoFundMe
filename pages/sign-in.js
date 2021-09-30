2163d806563306b81c245866ea0b10171bdbcd42
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-30 22:33:45 +0000

    refactored code, added server side rendering

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,37 +1,32 @@
-import React from 'react'
-import AuthNavBar from '../components/auth/auth-navbar'
-import SignInForm from '../components/auth/SignInForm'
-import Head from "next/head"
-
+import React from "react";
+import AuthNavBar from "../components/auth/auth-navbar";
+import SignInForm from "../components/auth/SignInForm";
+import Head from "next/head";
+import cookie from "cookie";
 
 const SignIn = () => {
-    return (
-        <>
-            <Head>
-                <title>
-                    Sign In | GoFundMe
-                </title>
-                
-            </Head>
-            
-            <div className="text-primary_black relative lg:py-40 font-primary bg-gray-100 min-w-full min-h-screen">
+  return (
+    <>
+      <Head>
+        <title>Sign In | GoFundMe</title>
+      </Head>
 
-                <AuthNavBar/>
+      <div className="text-primary_black relative lg:py-40 font-primary bg-gray-100 min-w-full min-h-screen">
+        <AuthNavBar />
 
-                <SignInForm/>
-    
-            </div>
+        <SignInForm />
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
+export const getServerSideProps = ({ req, res }) => {
+  const { refresh_token } = cookie.parse(req.headers.cookie);
 
-export const getServerSideProps=({req,res})=>{
-    const {refresh_token} = cookie.parse(req.headers.cookie)
+  if (refresh_token)
+    return { redirect: { permanent: false, destination: "/" } };
 
-    if (refresh_token) return {redirect:{path:"/"}}
+  return { props: {} };
+};
 
-    return {props:{}}
-}
-
-export default SignIn
+export default SignIn;
