c2ff0506aa7d542e8f77e070358e84b50a846205
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-30 22:32:27 +0000

    refactored code

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,20 @@
-import 'tailwindcss/tailwind.css'
-import "../components/assets/style.css"
-import NextNProgress from 'nextjs-progressbar'
+import "tailwindcss/tailwind.css";
+import "../components/assets/style.css";
+import NextNProgress from "nextjs-progressbar";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import AuthContext from '../context/AuthContext';
-
+import AuthContext from "../context/AuthContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <AuthContext>
-
-        <NextNProgress color="#02a95c" />
-        <ToastContainer/>
-        <Component {...pageProps} />
-
-      </AuthContext>
-        )
+    <AuthContext>
+      <NextNProgress color="#02a95c" />
+      <ToastContainer />
+      <Component {...pageProps} />
+    </AuthContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
