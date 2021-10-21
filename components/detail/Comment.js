fd4cbeecd403bc253f692eb137aa0e1a10f71fc4
Oliver Otchere <ootchere@payswitch.com.gh>
2021-10-21 15:37:11 +0000

    minor changes

--- components/detail/Comment.js
+++ components/detail/Comment.js
@@ -3,9 +3,9 @@ import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-const Comment = ({comment}) => {
+const Comment = ({comment,removeBorderTop}) => {
     return (
-        <div className={` my-2 border-t py-2 text-green-700 items-center`}>
+        <div className={` my-2 ${ !removeBorderTop && "border-t"} py-2 text-green-700 items-center`}>
             <div className="flex items-center">
                 <div className="flex justify-center  items-center h-10 w-10 rounded-full mr-4 bg-green-100">
                     <FontAwesomeIcon icon={ faUser } />
