7d3845389ac4b2d6cb7e0125d4697db3ffec19eb
Oliver Otchere <ootchere@payswitch.com.gh>
2021-10-28 17:35:04 +0000

    context with redux int

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,115 +1,103 @@
-import { useRouter } from 'next/router'
-import React, { createContext, useState } from 'react'
-import { useEffect } from 'react'
-import { toast } from 'react-toastify'
+import { useRouter } from "next/router";
+import React, { createContext, useState } from "react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { registerUser, removeUser } from "../store/slices/userSlice";
 
+export const AuthContext = createContext();
 
+const AuthContextProvider = ({ children }) => {
+  const [authReady, setAuthReady] = useState(false);
+  const [user, setUser] = useState(null);
+  const [authError, setAuthError] = useState(null);
 
-export const AuthContext = createContext()
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    checkUserLoggedIn().then((data) => {
+      setAuthReady(true);
+      if (data) {
+        setUser(() => data?.full_name);
 
-const AuthContextProvider = ({children}) => {
+        dispatch(registerUser(data?.full_name));
+      }
+    });
+  }, []);
 
-    const [authReady,setAuthReady] =useState(false)
-    const [user,setUser] = useState(null)
-    const [authError, setAuthError] = useState(null)
+  const router = useRouter();
 
-    useEffect(()=>{
-        checkUserLoggedIn()
-            .then(data=>{
+  const login = async (body) => {
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-                setAuthReady(true)
-                if (data) setUser(data)
-            })
-        
-    },[])
+    const data = await res.json();
 
-    const router = useRouter()
+    if (!res.ok) return toast.error(data.message);
 
-    const login=async (body)=>{
+    toast.success("Login succedded");
 
-        const res = await fetch("/api/login",{
-            method:"POST",
-            headers:{
-                "content-type" : "application/json",
-            },
-            body:JSON.stringify(body)
+    return router.push("/");
+  };
 
-        })
+  const signup = async (body) => {
+    const res = await fetch("/api/sign-up", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        if (!res.ok) return toast.error(data.message)
+    if (!res.ok) return data.message.forEach((message) => toast.error(message));
 
-        // console.log(data)
+    toast.success("Sign-up succedded...Please Log In!!");
+    return router.push("/sign-in");
+  };
 
-        toast.success("Login succedded")
+  const logout = async () => {
+    const res = await fetch("/api/logout", {
+      method: "POST",
+    });
 
-        return router.push('/')
-
-
-        
-
+    if (res.ok) {
+      setUser(null);
+      dispatch(removeUser());
     }
+    toast.info("Successfully logged out");
+  };
 
-    const signup=async(body)=>{
-        const res = await fetch("/api/sign-up",{
-            method:"POST",
-            headers:{
-                "content-type" : "application/json",
-            },
-            body:JSON.stringify(body)
+  const checkUserLoggedIn = async () => {
+    const res = await fetch("/api/current-user", {
+      method: "POST",
+    });
 
-        })
+    if (res.ok) {
+      const data = await res.json();
 
-        const data = await res.json()
-
-        if (!res.ok) return data.message.forEach(message=>toast.error(message))
-
-        console.log(data)
-
-        toast.success("Sign-up succedded...Please Log In!!")
-        return router.push("/sign-in")
+      return data;
     }
+  };
 
-    const logout=async()=>{
+  const clearUser = () => {
+    setUser(null);
+    dispatch(removeUser());
+  };
 
-        const res = await fetch("/api/logout",{
-            method:"POST"
-        }) 
+  return (
+    <AuthContext.Provider
+      value={{ login, user, signup, logout, checkUserLoggedIn, setUser }}
+    >
+      {authReady && children}
+    </AuthContext.Provider>
+  );
+};
 
-        if (res.ok) setUser(null)
-        toast.info("Successfully logged out")
-
-    }
-    
-
-    const checkUserLoggedIn=async()=>{
-        const res= await fetch("/api/current-user",{
-            method:"POST"
-        })
-
-        
-        if (res.ok){
-            const data = await res.json()
-            
-            return data
-        }
-
-       
-    }
-
-    const clearUser=()=>{
-        setUser(null)
-    }
-
-
-
-    return (
-        <AuthContext.Provider value={{login,user,signup,logout,checkUserLoggedIn,setUser}} >
-            {authReady && children}
-        </AuthContext.Provider>
-    )
-}
-
-export default AuthContextProvider
+export default AuthContextProvider;
