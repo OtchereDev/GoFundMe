cc46e6b0ae706020c1cd8eb235898629d7212316
Oliver Otchere <ootchere@payswitch.com.gh>
2021-11-02 17:15:39 +0000

    finished form 1 and it Validations

--- components/create/Form1.jsx
+++ components/create/Form1.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React,{useState} from 'react'
 import CountrySelector from './CountrySelect'
 import CategorySelect from './CategorySelect'
 import RaiseForSelect from './RaiseForSelect'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 
-const Form1 = () => { 
+const Form1 = ({increaseStep, formData, setFormData}) => { 
+
+  const [amount,setAmount] =useState(1)
+  const [title, setTitle] = useState("")
+  const [raiseFor,setRaiseFor] = useState()
+  const [beneficiary, setBeneficiary] = useState("")
+  const [location, setLocation] = useState()
+  const [category, setCategory] = useState() 
+
+
+  const [maxTitle] = useState(90)
+  const [allowNext, setAllowNext] = useState(false)
+
+  const handleAmount=(e)=>{
+
+    if(e.target.value.length>0 && parseInt(e.target.value)>0){
+      setAmount(parseInt(e.target.value.replace(/[^0-9]+/g, "")))
+    }
+    else if (!e.target.value.length ) {
+      setAmount("")
+    }
+    else{
+      setAmount(1)
+    }
+  }
+
+  const handleTitle=(e)=>{
+    if (maxTitle >=e.target.value.length ){
+      setTitle(e.target.value)
+    }
+  }
+
+  const handleNext=()=>{
+    if (!amount.toString().length) 
+      return toast.error("Please provide an amount to raised")
+    if (!title.length)
+      return toast.error("Please provide a fundraiser title")
+    if (!raiseFor)
+      return toast.error("Please select who you are raising the funds for")
+    if (raiseFor == 'Someone' && !beneficiary.length )
+      return toast.error("Please provide who you are raising the fund for")
+    if (!location)
+      return toast.error("Please provide the location for this fundraiser")
+    if (!category)
+      return toast.error("Please select a category for the fundraiser")
+
+    const data ={
+      amount,
+      title,
+      raiseFor,
+      beneficiary,
+      location,
+      category
+    }
+
+    setFormData(data)
+    increaseStep()
+  }
+
+  const handleInit=()=>{
+    setAmount(formData.amount)
+    setTitle(formData.title)
+    setRaiseFor(formData.raiseFor)
+    setBeneficiary(formData.beneficiary)
+    setLocation(formData.location)
+    setCategory(formData.category)
+  }
+
+  useEffect(()=>{
+    formData && handleInit()
+  },[])
   
   return (
     <div>
@@ -16,11 +88,10 @@ const Form1 = () => {
 
           <div className="flex items-center">
             <input
-              type="number"
-              min="0"
-              // value={donation}
-              // onChange={(e) => setDonation(e.target.value)}
-              // onFocus={(e) => setShowPayment(false)}
+              // type="number"
+              min="1"
+              value={amount}
+              onChange={handleAmount}
               className="block w-full text-4xl md:text-5xl font-semibold text-right outline-none appearance-none"
             />
             <h3 className="text-4xl md:text-5xl font-semibold text-gray-800 mb-0">
@@ -35,8 +106,8 @@ const Form1 = () => {
 
         <div className="flex items-center w-full">
           <input
-            // value={donation}
-            // onChange={(e) => setDonation(e.target.value)}
+            value={title}
+            onChange={handleTitle}
             // onFocus={(e) => setShowPayment(false)}
             placeholder='Campaigne title'
             className="block w-full text-lg  outline-none appearance-none placeholder-gray-300"
@@ -44,7 +115,7 @@ const Form1 = () => {
         </div>
       
           <h3 className=" text-lg font-semibold text-gray-800 mb-0 px-2">
-           50
+           {maxTitle - title.length}
           </h3>
       </div>
       {/* <div>
@@ -57,15 +128,24 @@ const Form1 = () => {
         </select>
       </div> */}
 
-      <RaiseForSelect/>
+      <RaiseForSelect setRaiseFor={setRaiseFor} raiseFor={raiseFor} />
 
-      <div>
-        <input className='w-full p-2 block outline-none border rounded-md' placeholder='Enter name of Beneficatory'  />
-      </div>
+      {
+        raiseFor == "Someone"
+        &&
+        <div>
+          <input 
+            className='w-full p-2 block outline-none border rounded-md' 
+            placeholder='Enter name of Beneficatory'
+            value={beneficiary}
+            onChange={e=>setBeneficiary(e.target.value)}  />
+        </div>
+      
+      }
 
-      <CountrySelector/>
+      <CountrySelector location={location} setLocation={setLocation} />
 
-      <CategorySelect/>
+      <CategorySelect category={category} setCategory={setCategory}  />
 
       <div>
         <p className='text-sm text-gray-400'>
@@ -73,12 +153,13 @@ const Form1 = () => {
         </p>
       </div>
 
-      <div className='my-2'>
-        <button className='w-full text-lg font-semibold bg-primary_green py-3 text-gray-50 rounded-md '>
-          Next
-        </button>
-      </div>
-
+      
+        <div className='my-2'>
+          <button onClick={handleNext} className='w-full text-lg font-semibold bg-primary_green py-3 text-gray-50 rounded-md '>
+            Next
+          </button>
+        </div>
+   
  
       
   </div>
