2a83c7c1429dd6aad6ee70f17aa5e2f7584bf18c
Oliver Otchere <oliverotchere4@gmail.com>
2021-11-16 16:48:31 +0000

    added graphql to app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'antd/es/modal/style/index.css'
 import 'antd/es/button/style/index.css'
 import 'antd/es/slider/style/index.css';
+import 'antd/es/spin/style/index.css';
 import 'antd/es/timeline/style/index.css'
 import "tailwindcss/tailwind.css";
 import "../components/assets/style.css";
@@ -13,16 +14,32 @@ import AuthContext from "../context/AuthContext";
 
 import { store } from "../store/store";
 import { Provider } from "react-redux";
+import { APIURL } from '../config/config';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: `${APIURL}/graphql`,
+  cache: new InMemoryCache()
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <AuthContext>
-        <NextNProgress color="#02a95c" />
-        <ToastContainer />
-        <Component {...pageProps} />
-      </AuthContext>
-    </Provider>
+    <ApolloProvider client={client}>
+
+      <Provider store={store}>
+        <AuthContext>
+          <NextNProgress color="#02a95c" />
+          <ToastContainer />
+          <Component {...pageProps} />
+        </AuthContext>
+      </Provider>
+
+    </ApolloProvider>
   );
 }
 
